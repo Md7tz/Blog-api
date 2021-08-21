df71dfb2a306adbfaff2b9c3a2750ef91e618413
Medhat Usama <medhatusama@gmail.com>
2021-08-21 18:49:59 +0300

    permissions added

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions # , permissions
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
 # read/write => ListCreateAPIView
 class PostList(generics.ListCreateAPIView):
+  # permission_classes = (permissions.IsAuthenticated,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
 # read/write/delete => RetrieveUpdateDestroyAPIView
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  # permission_classes = (permissions.IsAuthenticated,)
+  permission_classes = (IsAuthorOrReadOnly,)
   queryset = Post.objects.all()
   serializer_class = PostSerializer
